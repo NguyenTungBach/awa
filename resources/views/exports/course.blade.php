b6669db6b8da01c9abac72ff35878443bfc58658
HaiYen0110 <haiyentp.1204@gmail.com>
2023-08-21 17:39:59 +0700

    fix bug

--- resources/views/exports/course.blade.php
+++ resources/views/exports/course.blade.php
@@ -3,16 +3,8 @@
     $styleHeader = "border-bottom: 1px solid #000; font-size: 11pt";
 @endphp
 
-<style>
-    .auto-size {
-        table-layout: auto;
-        width: 100%;
-        word-wrap: break-word;
-    }
-</style>
-
 <div>
-    <table class="auto-size">
+    <table>
         <thead>
             <tr>
                 <th colspan="2" style="width: 60px">運行情報一覧</th>
@@ -25,13 +17,13 @@
         <tbody>
             <tr>
                 @foreach (config('courses.header_export') as $key => $item)
-                    <th>{{ $item }}</th>
+                    <th style="min-width: 80px">{{ $item }}</th>
                 @endforeach
             </tr>
             @foreach ($result as $key => $item)
                 <tr>
                     @foreach ($item as $k => $v)
-                        <td style="{{ $styleDefault }}" >{{ $v }}</td>
+                        <td style="min-width: 80px;{{ $styleDefault }}" >{{ $v }}</td>
                     @endforeach;
                 </tr>
             @endforeach
