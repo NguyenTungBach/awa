7305d728c5f2878343abb7760652297e8cd2274d
HaiYen0110 <haiyentp.1204@gmail.com>
2023-08-21 18:02:29 +0700

    fix bug

--- resources/views/exports/course.blade.php
+++ resources/views/exports/course.blade.php
@@ -16,14 +16,26 @@
         </thead>
         <tbody>
             <tr>
-                @foreach (config('courses.header_export') as $key => $item)
-                    <th style="min-width: 80px">{{ $item }}</th>
-                @endforeach
+                <th>ID</th>
+                <th>運行日</th>
+                <th>運行名</th>
+                <th style="width: 100px;">始業時間</th>
+                <th style="width: 100px;">終業時間</th>
+                <th style="width: 120px;">休憩時間</th>
+                <th style="width: 120px;">荷主名</th>
+                <th style="width: 120px;">発地</th>
+                <th style="width: 120px;">着地</th>
+                <th style="width: 120px;">運賃</th>
+                <th style="width: 180px;">協力会社支払金額</th>
+                <th style="width: 180px;">高速道路・フェリー料金</th>
+                <th style="width: 120px;">歩合</th>
+                <th style="width: 120px;">食事補助金額</th>
+                <th style="width: 120px;">メモ</th>
             </tr>
             @foreach ($result as $key => $item)
                 <tr>
                     @foreach ($item as $k => $v)
-                        <td style="min-width: 80px;{{ $styleDefault }}" >{{ $v }}</td>
+                        <td style="{{ $styleDefault }}" >{{ $v }}</td>
                     @endforeach;
                 </tr>
             @endforeach
