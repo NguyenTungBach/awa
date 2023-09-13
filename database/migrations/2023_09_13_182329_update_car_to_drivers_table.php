a33d618144880868b999cc4c7511669b00d6269c
bach nguyen <nguyentungbachholo@gmail.com>
2023-09-13 16:48:28 +0700

    bach update logic shift show driver

--- database/migrations/2023_09_13_182329_update_car_to_drivers_table.php
+++ database/migrations/2023_09_13_182329_update_car_to_drivers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateStartDateToDriversTable extends Migration
+class UpdateCarToDriversTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class UpdateStartDateToDriversTable extends Migration
     public function up()
     {
         Schema::table('drivers', function (Blueprint $table) {
-            $table->dateTime('start_date')->comment('day join company')->nullable(true)->change();
+            $table->string('car', 20)->nullable(false)->change();
         });
     }
 
@@ -26,7 +26,7 @@ class UpdateStartDateToDriversTable extends Migration
     public function down()
     {
         Schema::table('drivers', function (Blueprint $table) {
-            $table->dateTime('start_date')->comment('day join company')->nullable(false)->change();
+            $table->string('car', 20)->nullable(true)->change();
         });
     }
 }
--- database/seeders/DriverSeeder.php
+++ database/seeders/DriverSeeder.php
@@ -23,7 +23,7 @@ class DriverSeeder extends Seeder
                 'driver_name' => 'Manager',
                 'type' => 1,
                 'start_date' => '2022-06-16',
-                'car' => 'Truck',
+                'car' => '0011',
                 'status' => 1,
             ],
             [
@@ -31,7 +31,7 @@ class DriverSeeder extends Seeder
                 'driver_name' => 'Part-time',
                 'type' => 2,
                 'start_date' => '2022-07-17',
-                'car' => 'Container',
+                'car' => '0022',
                 'status' => 1,
             ],
             [
@@ -39,7 +39,7 @@ class DriverSeeder extends Seeder
                 'driver_name' => 'Full time',
                 'type' => 3,
                 'start_date' => '2022-08-18',
-                'car' => 'Labo',
+                'car' => '0033',
                 'status' => 1,
             ],
             [
@@ -47,7 +47,7 @@ class DriverSeeder extends Seeder
                 'driver_name' => 'Associate company',
                 'type' => 4,
                 'start_date' => '2022-09-19',
-                'car' => 'Grap',
+                'car' => '0044',
                 'status' => 1,
             ],
         ]);
