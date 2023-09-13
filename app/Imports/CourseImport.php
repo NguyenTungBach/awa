8b3eb7085d458e0c03c15eab42ca2abd85a109b5
HaiYen0110 <haiyentp.1204@gmail.com>
2023-09-13 18:09:02 +0700

    update import course

--- app/Imports/CourseImport.php
+++ app/Imports/CourseImport.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\DB;
 use App\Rules\CourseRule;
 use App\Models\Customer;
 use App\Models\Course;
+use App\Models\Driver;
 use App\Rules\CompareHours;
 use Repository\CourseRepository;
 
@@ -37,6 +38,27 @@ class CourseImport implements ToCollection, WithHeadings, WithStartRow, WithVali
             '歩合', // 11 commission
             '食事補助金額', // 12 meal_fee
             'メモ', // 13 note
+
+
+            '運行日', // 0 ship_date 0
+            '従業員名', // 1 driver_id
+            '車両番号', // 2 vehicle_number
+            '始業時間', // 3 start_date 2
+            '終業時間', // 4 end_date 3
+            '休憩時間', // 5 break_time 4
+            '荷主名', // 6 customer_id 5
+            '発地', // 7 departure_place 6
+            '着地', // 8 arrival_place 7
+            '品名', // 9 item_name
+            '数量', // 10 quantity
+            '単価', // 11 price
+            '重量', // 12 weight
+            '運賃', // 13 ship_fee 8
+            '協力会社支払金額', // 14 associate_company_fee 9
+            '高速道路・フェリー料金', // 15 expressway_fee 10
+            '歩合', // 16 commission 11
+            '食事補助金額', // 17 meal_fee 12
+            'メモ', // 18 note 13
         ];
     }
 
@@ -59,44 +81,56 @@ class CourseImport implements ToCollection, WithHeadings, WithStartRow, WithVali
             // 0 ship_date
             '*.0' => ['required', 'date_format:Y-m-d', new CourseRule($this->headings()[0])],
 
-            // 1 course_name
-            '*.1'  => ['required', 'string', 'max:20', 'unique:courses,course_name,NULL,id,deleted_at,NULL',],
+            // 1 driver_id
 
-            // 2 start_date
-            '*.2' => ['required', 'date_format:H:i'],
+            // // 1 course_name
+            // '*.1'  => ['required', 'string', 'max:20', 'unique:courses,course_name,NULL,id,deleted_at,NULL',],
 
-            // 3 end_date
+            // 2 vehicle_number
+
+            // 3 start_date
             '*.3' => ['required', 'date_format:H:i'],
 
-            // 4 break_time
+            // 4 end_date
             '*.4' => ['required', 'date_format:H:i'],
 
-            // 5 customer_id
-            '*.5' => ['required', Rule::in($arrCustomerName)],
+            // 5 break_time
+            '*.5' => ['required', 'date_format:H:i'],
 
-            // 6 departure_place
-            '*.6' => ['required', 'string', 'max:20'],
+            // 6 customer_id
+            '*.6' => ['required', Rule::in($arrCustomerName)],
 
-            // 7 arrival_place
+            // 7 departure_place
             '*.7' => ['required', 'string', 'max:20'],
 
-            // 8 ship_fee
-            '*.8' => ['required', 'numeric'],
+            // 8 arrival_place
+            '*.8' => ['required', 'string', 'max:20'],
 
-            // 9 associate_company_fee
-            '*.9' => ['nullable', 'numeric'],
+            // 9 item_name
 
-            // 10 expressway_fee
-            '*.10' => ['nullable', 'numeric'],
+            // 10 quantity
 
-            // 11 commission
-            '*.11' => ['nullable', 'numeric'],
+            // 11 price
 
-            // 12 meal_fee
-            '*.12' => ['nullable', 'numeric'],
+            // 12 weight
 
-            // 13 note
-            '*.13' => ['nullable', 'string', 'max:1000'],
+            // 13 ship_fee
+            '*.13' => ['required', 'numeric'],
+
+            // 14 associate_company_fee
+            '*.14' => ['nullable', 'numeric'],
+
+            // 15 expressway_fee
+            '*.15' => ['nullable', 'numeric'],
+
+            // 16 commission
+            '*.16' => ['nullable', 'numeric'],
+
+            // 17 meal_fee
+            '*.17' => ['nullable', 'numeric'],
+
+            // 18 note
+            '*.18' => ['nullable', 'string', 'max:1000'],
 
         ];
 
@@ -113,53 +147,73 @@ class CourseImport implements ToCollection, WithHeadings, WithStartRow, WithVali
             '*.0.required'  => __('validation.custom.csv.required', ['attribute' => $this->headings()[0]]),
             '*.0.date_format'  => __('validation.custom.csv.date_format', ['attribute' => $this->headings()[0], 'format' => 'Y-m-d']),
 
-            // 1 course_name
-            '*.1.required'  => __('validation.custom.csv.required', ['attribute' => $this->headings()[1]]),
-            '*.1.string'  => __('validation.custom.csv.string', ['attribute' => $this->headings()[1]]),
-            '*.1.max'  => __('validation.custom.csv.max', ['attribute' => $this->headings()[1], 'max' => 20]),
-            '*.1.unique'  => __('validation.custom.csv.unique', ['attribute' => $this->headings()[1]]),
-            // 2 start_date
-            '*.2.required'  => __('validation.custom.csv.required', ['attribute' => $this->headings()[2]]),
-            '*.2.date_format'  => __('validation.custom.csv.date_format', ['attribute' => $this->headings()[2], 'format' => 'H:i']),
+            // // 1 course_name
+            // '*.1.required'  => __('validation.custom.csv.required', ['attribute' => $this->headings()[1]]),
+            // '*.1.string'  => __('validation.custom.csv.string', ['attribute' => $this->headings()[1]]),
+            // '*.1.max'  => __('validation.custom.csv.max', ['attribute' => $this->headings()[1], 'max' => 20]),
+            // '*.1.unique'  => __('validation.custom.csv.unique', ['attribute' => $this->headings()[1]]),
 
-            // 3 end_date
+            // 1 driver_id
+
+            // 2 vehicle_number
+
+            // 3 start_date
             '*.3.required'  => __('validation.custom.csv.required', ['attribute' => $this->headings()[3]]),
             '*.3.date_format'  => __('validation.custom.csv.date_format', ['attribute' => $this->headings()[3], 'format' => 'H:i']),
 
-            // 4 break_time
+            // 4 end_date
             '*.4.required'  => __('validation.custom.csv.required', ['attribute' => $this->headings()[4]]),
             '*.4.date_format'  => __('validation.custom.csv.date_format', ['attribute' => $this->headings()[4], 'format' => 'H:i']),
 
-            // 5 customer_id
+            // 5 break_time
             '*.5.required'  => __('validation.custom.csv.required', ['attribute' => $this->headings()[5]]),
-            '*.5.in'  => __('validation.custom.csv.in', ['attribute' => $this->headings()[5]]),
+            '*.5.date_format'  => __('validation.custom.csv.date_format', ['attribute' => $this->headings()[5], 'format' => 'H:i']),
 
-            // 6 departure_place
+            // 6 customer_id
             '*.6.required'  => __('validation.custom.csv.required', ['attribute' => $this->headings()[6]]),
-            '*.6.string'  => __('validation.custom.csv.string', ['attribute' => $this->headings()[6]]),
-            '*.6.max'  => __('validation.custom.csv.max', ['attribute' => $this->headings()[6], 'max' => 20]),
-            // 7 arrival_place
+            '*.6.in'  => __('validation.custom.csv.in', ['attribute' => $this->headings()[6]]),
+
+            // 7 departure_place
             '*.7.required'  => __('validation.custom.csv.required', ['attribute' => $this->headings()[7]]),
             '*.7.string'  => __('validation.custom.csv.string', ['attribute' => $this->headings()[7]]),
             '*.7.max'  => __('validation.custom.csv.max', ['attribute' => $this->headings()[7], 'max' => 20]),
-            // 8 ship_fee
+
+            // 8 arrival_place
             '*.8.required'  => __('validation.custom.csv.required', ['attribute' => $this->headings()[8]]),
-            '*.8.numeric'  => __('validation.custom.csv.numeric', ['attribute' => $this->headings()[8]]),
-            // 9 associate_company_fee
-            '*.9.required'  => __('validation.custom.csv.required', ['attribute' => $this->headings()[9]]),
-            '*.9.numeric'  => __('validation.custom.csv.numeric', ['attribute' => $this->headings()[9]]),
-            // 10 expressway_fee
-            '*.10.required'  => __('validation.custom.csv.required', ['attribute' => $this->headings()[10]]),
-            '*.10.numeric'  => __('validation.custom.csv.numeric', ['attribute' => $this->headings()[10]]),
-            // 11 commission
-            '*.11.required'  => __('validation.custom.csv.required', ['attribute' => $this->headings()[11]]),
-            '*.11.numeric'  => __('validation.custom.csv.numeric', ['attribute' => $this->headings()[11]]),
-            // 12 meal_fee
-            '*.12.required'  => __('validation.custom.csv.required', ['attribute' => $this->headings()[12]]),
-            '*.12.numeric'  => __('validation.custom.csv.numeric', ['attribute' => $this->headings()[12]]),
-            // 13 note
-            '*.13.string'  => __('validation.custom.csv.string', ['attribute' => $this->headings()[13]]),
-            '*.13.max'  => __('validation.custom.csv.max', ['attribute' => $this->headings()[13], 'max' => 1000]),
+            '*.8.string'  => __('validation.custom.csv.string', ['attribute' => $this->headings()[8]]),
+            '*.8.max'  => __('validation.custom.csv.max', ['attribute' => $this->headings()[8], 'max' => 20]),
+
+            // 9 item_name
+
+            // 10 quantity
+
+            // 11 price
+
+            // 12 weight
+
+            // 13 ship_fee
+            '*.13.required'  => __('validation.custom.csv.required', ['attribute' => $this->headings()[13]]),
+            '*.13.numeric'  => __('validation.custom.csv.numeric', ['attribute' => $this->headings()[13]]),
+
+            // 14 associate_company_fee
+            '*.14.required'  => __('validation.custom.csv.required', ['attribute' => $this->headings()[14]]),
+            '*.14.numeric'  => __('validation.custom.csv.numeric', ['attribute' => $this->headings()[14]]),
+
+            // 15 expressway_fee
+            '*.15.required'  => __('validation.custom.csv.required', ['attribute' => $this->headings()[15]]),
+            '*.15.numeric'  => __('validation.custom.csv.numeric', ['attribute' => $this->headings()[15]]),
+
+            // 16 commission
+            '*.16.required'  => __('validation.custom.csv.required', ['attribute' => $this->headings()[16]]),
+            '*.16.numeric'  => __('validation.custom.csv.numeric', ['attribute' => $this->headings()[16]]),
+
+            // 17 meal_fee
+            '*.17.required'  => __('validation.custom.csv.required', ['attribute' => $this->headings()[17]]),
+            '*.17.numeric'  => __('validation.custom.csv.numeric', ['attribute' => $this->headings()[17]]),
+
+            // 18 note
+            '*.18.string'  => __('validation.custom.csv.string', ['attribute' => $this->headings()[18]]),
+            '*.18.max'  => __('validation.custom.csv.max', ['attribute' => $this->headings()[18], 'max' => 1000]),
         ];
     }
 
@@ -170,6 +224,8 @@ class CourseImport implements ToCollection, WithHeadings, WithStartRow, WithVali
     {
         $arrCollection = $collection->toArray();
         $arrCustomer = $this->getCustomerId();
+        $arrDriver = $this->getDriverId();
+        // $arrVehicle = $this->getVehicleNumber();
         $dataImport = [];
         $errors = [];
         $result = [];
@@ -181,26 +237,32 @@ class CourseImport implements ToCollection, WithHeadings, WithStartRow, WithVali
                 $rowIndex = $i + $this->startRow();
 
                 // check start_time >= end_time
-                $start_time = date('H:i', strtotime('today '.$row[2]));
-                $end_time = date('H:i', strtotime('today '.$row[3]));
+                $start_time = date('H:i', strtotime('today '.$row[3]));
+                $end_time = date('H:i', strtotime('today '.$row[4]));
                 if ($start_time >= $end_time) {
                     $errors[] = __('validation.custom.csv.compare_date', ['row' => $rowIndex]);
                 }
 
-                $dataImport['customer_id'] = array_search($row[5], $arrCustomer);
-                $dataImport['course_name'] = $row[1];
+                $dataImport['customer_id'] = array_search($row[6], $arrCustomer);
+                $dataImport['course_name'] = 'driver ' . array_search($row[1], $arrDriver);
                 $dataImport['ship_date'] = $row[0];
-                $dataImport['start_date'] = $row[2];
-                $dataImport['end_date'] = $row[3];
-                $dataImport['break_time'] = $row[4];
-                $dataImport['departure_place'] = $row[6];
-                $dataImport['arrival_place'] = $row[7];
-                $dataImport['ship_fee'] = $row[8];
-                $dataImport['associate_company_fee'] = empty($row[9]) ? 0 : $row[9];
-                $dataImport['expressway_fee'] = empty($row[10]) ? 0 : $row[10];
-                $dataImport['commission'] = empty($row[11]) ? 0 : $row[11];
-                $dataImport['meal_fee'] = empty($row[12]) ? 0 : $row[12];
-                $dataImport['note'] = $row[13];
+                $dataImport['driver_id'] = array_search($row[1], $arrDriver);
+                // $dataImport['vehicle_number'] = $row[2];
+                $dataImport['start_date'] = $row[3];
+                $dataImport['end_date'] = $row[4];
+                $dataImport['break_time'] = $row[5];
+                $dataImport['departure_place'] = $row[7];
+                $dataImport['arrival_place'] = $row[8];
+                $dataImport['item_name'] = $row[9];
+                $dataImport['quantity'] = empty($row[10]) ? 0 : $row[10];
+                $dataImport['price'] = empty($row[11]) ? 0 : $row[11];
+                $dataImport['weight'] = empty($row[12]) ? 0 : $row[12];
+                $dataImport['ship_fee'] = $row[13];
+                $dataImport['associate_company_fee'] = empty($row[14]) ? 0 : $row[14];
+                $dataImport['expressway_fee'] = empty($row[15]) ? 0 : $row[15];
+                $dataImport['commission'] = empty($row[16]) ? 0 : $row[16];
+                $dataImport['meal_fee'] = empty($row[17]) ? 0 : $row[17];
+                $dataImport['note'] = $row[18];
 
                 if (!count($errors)) {
                     $result = Course::create($dataImport);
@@ -229,4 +291,26 @@ class CourseImport implements ToCollection, WithHeadings, WithStartRow, WithVali
 
         return $result;
     }
+
+    public function getDriverId()
+    {
+        $drivers = Driver::get();
+        $result = [];
+        foreach ($drivers as $key => $value) {
+            $result[$value->id] = $value->driver_name;
+        }
+
+        return $result;
+    }
+
+    // public function getVehicleNumber()
+    // {
+    //     $drivers = Driver::get();
+    //     $result = [];
+    //     foreach ($customers as $key => $value) {
+    //         $result[$value->car] = $value->driver_name;
+    //     }
+
+    //     return $result;
+    // }
 }
