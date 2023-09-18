9e407e3b43411b0307eecacb78cfdd4d3de7a849
bach nguyen <nguyentungbachholo@gmail.com>
2023-09-18 17:00:55 +0700

    bach update shift when get by driver by finnal closing

--- app/Http/Controllers/Api/DriverController.php
+++ app/Http/Controllers/Api/DriverController.php
@@ -146,8 +146,9 @@ class DriverController extends Controller
     {
         try {
             $request->merge(['status' => 1]);
-            $data = $this->repository->create($request->all());
-            return $this->responseJson(200, new DriverResource($data));
+//            $data = $this->repository->create($request->all());
+            return $this->repository->create($request->all());
+//            return $this->responseJson(200, new DriverResource($data));
         } catch (\Exception $e) {
             throw $e;
         }
@@ -297,8 +298,9 @@ class DriverController extends Controller
     public function update(DriverRequest $request, $id)
     {
         $attributes = $request->except([]);
-        $data = $this->repository->update($attributes, $id);
-        return $this->responseJson(200, new BaseResource($data));
+//        $data = $this->repository->update($attributes, $id);
+        return $this->repository->update($attributes, $id);
+//        return $this->responseJson(200, new BaseResource($data));
     }
 
     /**
