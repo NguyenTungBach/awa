dc392c77e89d560a6e931d1894c14287ea516692
dongvietlong <dongvietlong123@gmail.com>
2023-10-02 14:11:15 +0700

    check temporary

--- resources/js/const/modules/urlApi.js
+++ resources/js/const/modules/urlApi.js
@@ -80,6 +80,7 @@ const GET_DETAIL_SHIFT = '/driver-course';
 const UPDATE_SHIFT = '/driver-course/update-course';
 const GET_MESSAGE_RESPONSE_AI = '/shift/get-message-response-ai';
 const POST_TEMPORORY = '/temporary-closing';
+const GET_CHECK_BUTTON_TEMPORARY = '/temporary-closing/check-temporary';
 
 // Cash Disbusement
 const GET_LIST_CASH_DISBUSEMENT = '/driver-cash-out-statistical';
@@ -181,6 +182,7 @@ export default {
 	GET_MESSAGE_RESPONSE_AI,
 	POST_CLOSING_DATE,
 	POST_TEMPORORY,
+	GET_CHECK_BUTTON_TEMPORARY,
 
 	// Cash Disbusement
 	GET_LIST_CASH_DISBUSEMENT,
